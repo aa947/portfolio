bda9910ebc1e6e57f7c69bbd6b24e9ebdc285253
Ahmad Ali <aallii300300@gmail.com>
2020-01-22 20:10:15 +0000

    prepared to deploy to heroku

--- server.js
+++ server.js
@@ -81,13 +81,14 @@ app.get( '/api/footer', (req, res)=>{
   })
 });
 
+//uncomment this to deploy to heroku
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-  app.get('*', (req, res)=>{
-    res.sendFile( path.join(__dirname, 'client', 'build', 'index.html' ));
-  })
-}
+// if (process.env.NODE_ENV === 'production'){
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res)=>{
+//     res.sendFile( path.join(__dirname, 'client', 'build', 'index.html' ));
+//   })
+// }
 
 const port = process.env.PORT || 5000;
 
