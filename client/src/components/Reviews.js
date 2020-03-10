3989b0e60b3a7504121f83bff8564acb15863a19
Ahmad Ali <aallii300300@gmail.com>
2020-03-10 11:28:27 +0000

    reviews redesigned

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './css/Review.css';
-import { SocialIcon } from 'react-social-icons';
+// import { SocialIcon } from 'react-social-icons';
 import ReviewForm from './ReviewForm';
 
 
@@ -9,9 +9,9 @@ export default class Reviews extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        
-               reviews: []
-        
+
+            reviews: []
+
 
         };
 
@@ -21,11 +21,11 @@ export default class Reviews extends Component {
     componentDidMount() {
 
         fetch('/api/reviews').then(res => res.json())
-        .then(reviews => this.setState({ reviews }, () => {
-            console.log('Reviews fetched...', reviews)
-            console.log(this.state.reviews, 'dtste reviews');
-        }));
-       
+            .then(reviews => this.setState({ reviews }, () => {
+                // console.log('Reviews fetched...', reviews)
+                // console.log(this.state.reviews, 'dtste reviews');
+            }));
+
     }
 
 
@@ -38,35 +38,37 @@ export default class Reviews extends Component {
                 <hr className="testimonials" />
                 <p className="desc ">❤️</p>
 
-                <dv className="row tweetForm">
-                        <ReviewForm />
-                </dv>
-<br />
-
-                <div className="row" className="tweets">
-
-{ reviews.map((r)=>{return(
-    <div className="tweet">
-                
-                <div className="content">
-                    <p className="reviewText"> {r.message}</p>
-                    <p className="reviewAuthor"> {r.name}</p>
-                    <i className="relation" >{r.rel}</i>
-                
+                <div className="row tweetForm">
+                    <ReviewForm />
                 </div>
-               
+                <br />
+
+                <div  className=" row tweets">
+
+                    {reviews.map((r) => {
+                        return (
+                            <div className="tweet" key={ r._id}>
+
+                                <div className="content">
+                                    <p className="reviewText"> {r.message}</p>
+                                    <p className="reviewAuthor"> {r.name}</p>
+                                    <i className="relation" >{r.rel}</i>
+
+                                </div>
+
+                            </div>
+                        );
+                    })}
+
+
+
+                    <br />
+                </div>
+
+
             </div>
-);}) }
-                  
 
 
-                  <br />
-                </div>
-
-
-                </div>
-
-
-                )
-            }
-        }
+        )
+    }
+}
