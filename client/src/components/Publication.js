1c612c1003703205aa4e404bc87ec1bffa1fd41f
Ahmad Ali <aallii300300@gmail.com>
2020-04-16 04:31:47 +0100

    publications

---
+++ client/src/components/Publication.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+// import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
+//import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
+
+
+import './css/intro.css';
+import { Link } from 'react-router-dom';
+
+class Contact_form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+           
+
+
+        };
+
+       
+
+    }
+
+ 
+    render() {
+
+
+        return (
+
+
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="m-0 font-weight-bold text-primary">Publications:</h6>
+                </div>
+                <div className="card-body">
+
+                    <p style={{ marginBottom: "7%" }}><i className="fas fa-chevron-right"></i>{'\u00A0'}<i class="fab fa-youtube"></i> Site views counter with Node and MongoDB - tutorial on youtube <i><span style={{ fontSize: "12px" }}> published Feb 2020 </span></i>
+                        {'\u00A0'}{'\u00A0'}{'\u00A0'}
+                        <a href="https://www.youtube.com/playlist?list=PLrMI74uzoMfm4fFCCJvLrpYtV4U1A-6Kj" title="visit publication" target="_blank" className="btn-link text-muted">
+                            <i className="fas fa-external-link-alt"> visit</i>
+                        </a>
+                    </p>
+
+                    <p style={{ marginBottom: "7%" }}><i className="fas fa-chevron-right"></i>{'\u00A0'}<i class="fas fa-newspaper"></i> Promises or Callbacks .. key Notices in MongoDB - article on DEV.to <i><span style={{ fontSize: "12px" }}> published Jan 2020 </span></i>
+                        {'\u00A0'}{'\u00A0'}{'\u00A0'}
+                        <a href="https://dev.to/ahmad_ali/promises-or-callbacks-key-notices-in-mongodb-15po" title="visit publication" target="_blank" className="btn-link text-muted">
+                            <i className="fas fa-external-link-alt"> visit</i>
+                        </a>
+                    </p>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Contact_form;
