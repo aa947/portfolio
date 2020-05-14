9688628acab5e0a71eef849860517bb8a8830eeb
Ahmad Ali <aallii300300@gmail.com>
2020-05-15 00:42:20 +0100

    dynamic title try

---
+++ client/src/components/layout/Title.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import {Helmet} from 'react-helmet';
+
+const TitleComponent = ({ title }) => {
+    var defaultTitle = '⚛️ app';
+    return (
+        <Helmet>
+            <title>{title ? title : defaultTitle}</title>
+        </Helmet>
+    );
+};
+
+export { TitleComponent };
