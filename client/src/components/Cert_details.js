f2150b600982939bcffd8e51eea56ace320f50f5
Ahmad Ali <aallii300300@gmail.com>
2020-05-15 00:43:19 +0100

    dynamic title with helmet

--- client/src/components/Cert_details.js
+++ client/src/components/Cert_details.js
@@ -8,6 +8,8 @@ import './css/header.css';
 // import footer from './Footer';
 import Footer from './Footer';
 // import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import DocumentTitle from 'react-document-title';
 
 
 class Cert_details extends Component {
@@ -35,12 +37,64 @@ class Cert_details extends Component {
             }
             );
 
+
+        //meta stuff 
+        const head = document.querySelector('head');
+
+        const title = document.createElement('title');
+        title.innerText=this.state.cert.cert_name;
+        head.appendChild(title);
+  
+        const meta = document.createElement('meta');
+        meta.setAttribute('property', 'og:titel');
+        meta.setAttribute('content', 'Ahmad Ali - Full Stack Developer');
+        head.appendChild(meta);
+  
+        const meta1 = document.createElement('meta');
+        meta1.setAttribute('property', 'og:description');
+        meta1.setAttribute('content', 'Full-stack developer, back-ended with the precision of chemistry and medical practise.  Educated to degree level with 1 years’ experience in web development. This has given me skills in JS: MERN, PHP: WP_API, DB: MONGO, SQL, HTML and CSS.');
+        head.appendChild(meta1);
+  
+       
+  
+        const meta2 = document.createElement('meta');
+        meta2.setAttribute('property', 'og:image');
+        meta2.setAttribute('content', 'https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0');
+        head.appendChild(meta2);
+  
+        const meta3 = document.createElement('meta');
+        meta3.setAttribute('property', '"og:url');
+        meta3.setAttribute('content', "https://www.ahmad-ali.co.uk/");
+        head.appendChild(meta3);
+  
+        const meta4 = document.createElement('meta');
+        meta4.setAttribute('property', 'author');
+        meta4.setAttribute('content', 'Ahmad Ali');
+        head.appendChild(meta4);
+  
+  
+
     }
 
     render() {
 
 
         return (
+            <DocumentTitle title={`Ahmad - ${this.state.cert.cert_name}`}>
+            <>
+                <Helmet>
+                    <meta property="og:title" content="Ahmad Ali - Full Stack Developer" />
+                    <meta property="og:description" content="Full-stack developer, back-ended with the precision of chemistry and medical practise.  Educated to degree level with 1 years’ experience in web development. This has given me skills in JS: MERN, PHP: WP_API, DB: MONGO, SQL, HTML and CSS." />
+                    <meta property="og:image" content="https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0" />
+                        <meta property="og:url" content="https://www.ahmad-ali.co.uk/" />
+                        <meta name="author" content="Ahmad Ali" />
+                        <meta name="twitter:card" content="Ahmad Ali Full Stack Developer" />
+                        <meta name="twitter:image" content="https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0" />
+            </Helmet>
+
+
+
+
             <React.Fragment>
                 <div className="col-lg-12 mb-12">
                     <div className="card shadow mb-12">
@@ -98,6 +152,8 @@ class Cert_details extends Component {
                 <br />
                 <Footer />
             </React.Fragment >
+            </>
+</DocumentTitle>
         );
     }
 }
--- client/src/components/Contact_row.js
+++ client/src/components/Contact_row.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 //import Jumbotron from 'react-bootstrap/Jumbotron';
 import Contact_form from './Contact_form';
 import NewsLetter_form from './NewsLetter_form';
+import { Helmet } from 'react-helmet';
+import DocumentTitle from 'react-document-title';
 
 
 
@@ -20,30 +22,85 @@ class Contact_row extends Component {
         };
     }
 
+    componentWillMount() {
+
+    }
     componentDidMount() {
+
+      
+
+
+        //     // document.title = this.props.title;
+
+        //     const head = document.querySelector('head');
+
+        //    // const title = document.documentElement.getElementsByTagName('title');
+        //     // title.remove();
+        //    // title.innerText="Contact | Ahmad Ali";
+        //     // head.appendChild(title);
+
+        //     const meta = document.createElement('meta');
+        //     meta.setAttribute('property', 'og:titel');
+        //     meta.setAttribute('content', 'Ahmad Ali - Full Stack Developer');
+        //     head.appendChild(meta);
+
+        //     const meta1 = document.createElement('meta');
+        //     meta1.setAttribute('property', 'og:description');
+        //     meta1.setAttribute('content', 'Full-stack developer, back-ended with the precision of chemistry and medical practise.  Educated to degree level with 1 years’ experience in web development. This has given me skills in JS: MERN, PHP: WP_API, DB: MONGO, SQL, HTML and CSS.');
+        //     head.appendChild(meta1);
+
+
+
+        //     const meta2 = document.createElement('meta');
+        //     meta2.setAttribute('property', 'og:image');
+        //     meta2.setAttribute('content', 'https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0');
+        //     head.appendChild(meta2);
+
+        //     const meta3 = document.createElement('meta');
+        //     meta3.setAttribute('property', '"og:url');
+        //     meta3.setAttribute('content', "https://www.ahmad-ali.co.uk/");
+        //     head.appendChild(meta3);
+
+        //     const meta4 = document.createElement('meta');
+        //     meta4.setAttribute('property', 'author');
+        //     meta4.setAttribute('content', 'Ahmad Ali');
+        //     head.appendChild(meta4);
+
 
 
     }
 
     render() {
         return (
-            <div className="col-lg-12 mb-12">
+
+            <DocumentTitle title="Contact | Ahmad Ali ">
+            <>
+                <Helmet>
+                    <meta property="og:title" content="Ahmad Ali - Full Stack Developer" />
+                    <meta property="og:description" content="Full-stack developer, back-ended with the precision of chemistry and medical practise.  Educated to degree level with 1 years’ experience in web development. This has given me skills in JS: MERN, PHP: WP_API, DB: MONGO, SQL, HTML and CSS." />
+                    <meta property="og:image" content="https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0" />
+                        <meta property="og:url" content="https://www.ahmad-ali.co.uk/" />
+                        <meta name="author" content="Ahmad Ali" />
+                        <meta name="twitter:card" content="Ahmad Ali Full Stack Developer" />
+                        <meta name="twitter:image" content="https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0" />
+            </Helmet>
+
+                    <div className="col-lg-12 mb-12">
 
 
 
 
 
-<Contact_form />
+                        <Contact_form />
 
 
-<NewsLetter_form />
-
-               
-
-            </div>
+                        <NewsLetter_form />
 
 
 
+                    </div>
+</>
+</DocumentTitle>
         );
 
     }
