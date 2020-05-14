33fa4c797ff5af57c469a9f28c0623397bca2481
Ahmad Ali <aallii300300@gmail.com>
2020-05-15 00:33:03 +0100

    change skills section

--- client/src/components/Main_skills.js
+++ client/src/components/Main_skills.js
@@ -24,76 +24,62 @@ class Edu_col extends Component {
         return (
             <React.Fragment>
 
-                {/* skill Card */}
-                <div className="col-lg-4 mb-3 sm-6">
-                    <div className="card border shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Express</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">80%</div>
-                                        </div>
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-node-js fa-6x text-green-300"></i>
 
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fab fa-node-js fa-1.5x text-green-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
-                {/* skill Card */}
-                <div className="col-lg-4 mb-3 sm-6">
-                    <div className="card border shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-info text-uppercase mb-1">React</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                        </div>
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-react fa-6x text-green-300"></i>
 
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fab fa-react fa-1.5x text-green-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
 
-{/* skill Card */}
-<div className="col-lg-4 mb-3 sm-6">
-                    <div className="card border shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-info text-uppercase mb-1">MongoDB</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">80%</div>
-                                        </div>
+                <div className="col-sm-4 col-md-3 mb-3">
+                    {/* <i className="fas fa-database fa-6x text-green-300"></i> */}
+                    <img src={process.env.PUBLIC_URL + "/imgs/mongodb.png"} width="100vw" />
+                </div>
 
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fas fa-database fa-1.5x text-green-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-css3-alt fa-6x text-green-300"></i>
+                </div>
+
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fas fa-database fa-6x text-green-300"></i>
+                </div>
+
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-js-square fa-6x text-green-300"></i>
+                </div>
+
+               
+
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-html5 fa-6x text-green-300"></i>
+
+                </div>
+
+                
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-bootstrap fa-6x text-green-300"></i>
+                </div>
+
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-git-alt fa-6x text-green-300"></i>
+                </div>
+
+                <div className="col-sm-4 col-md-3 mb-3">
+                    {/* <i className="fas fa-database fa-6x text-green-300"></i> */}
+                    <img src={process.env.PUBLIC_URL + "/imgs/redux.jpg"} width="100vw" />
+                </div>
+                <div className="col-sm-4 col-md-3 mb-3">
+                    <i className="fab fa-php fa-6x text-green-300"></i>
                 </div>
 
 
-{/* skill Card */}
-<div className="col-lg-4 mb-3 sm-6">
+
+                {/* 
+<div className="col-sm-4 col-md-3 mb-3">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
                             <div className="row no-gutters align-items-center">
@@ -115,7 +101,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -138,7 +124,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -161,7 +147,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -184,7 +170,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -207,7 +193,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -230,7 +216,7 @@ class Edu_col extends Component {
                 </div>
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -254,7 +240,7 @@ class Edu_col extends Component {
 
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -278,7 +264,7 @@ class Edu_col extends Component {
 
 
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -300,7 +286,7 @@ class Edu_col extends Component {
                     </div>
                 </div>
 
-{/* skill Card */}
+
 <div className="col-lg-4 mb-3 sm-6">
                     <div className="card border shadow h-100 py-2">
                         <div className="card-body">
@@ -320,10 +306,10 @@ class Edu_col extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
 
-               
+
             </React.Fragment>
         );
 
