6de3692c35d39df6d855f2908a7ac22faecc96fe
Ahmad Ali <aallii300300@gmail.com>
2020-05-21 03:04:30 +0100

    clean newsletter

--- client/src/components/NewsLetter_form.js
+++ client/src/components/NewsLetter_form.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
-// import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-//import Jumbotron from 'react-bootstrap/Jumbotron';
 import axios from 'axios';
-import {  toast } from 'react-toastify';
-
-
-
+import { toast } from 'react-toastify';
 import './css/intro.css';
-// import { Link } from 'react-router-dom';
 
 class Contact_form extends Component {
     constructor(props) {
@@ -17,56 +9,47 @@ class Contact_form extends Component {
         this.state = {
             inputEmail: '',
             subscribers: ''
-
         };
 
         this.subscribe = this.subscribe.bind(this);
         this.handleChangeEmail = this.handleChangeEmail.bind(this);
-
     }
 
     subscribe(event) {
         event.preventDefault();
-       // console.log(this.state);
-       if(this.state.inputEmail == ''){
-        toast.error('please inter an email to subscribe',{
-           position: toast.POSITION.TOP_CENTER,
-           autoClose: false
-       })
-       return false;
-            } else {
-        axios.post('/api/sub', this.state)
-            .then((res) =>{
-             //console.log(res)
-          
-             this.setState({ inputEmail: '' });
-             toast.success('Thank You for subscribing to my Newsletter!', {
-                 position: toast.POSITION.TOP_CENTER,
-                 autoClose: false
-             });
+        // console.log(this.state);
+        if (this.state.inputEmail == '') {
+            toast.error('please inter an email to subscribe', {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: false
             })
-            .catch(err => console.log(err))
+            return false;
+        } else {
+            axios.post('/api/sub', this.state)
+                .then((res) => {
+                    //console.log(res) 
+                    this.setState({ inputEmail: '' });
+                    toast.success('Thank You for subscribing to my Newsletter!', {
+                        position: toast.POSITION.TOP_CENTER,
+                        autoClose: false
+                    });
+                })
+                .catch(err => console.log(err))
+        }
     }
-}
-
-    
 
     handleChangeEmail(event) {
         this.setState({ inputEmail: event.target.value })
     }
 
-   
-   
-        componentDidMount() {
-            fetch('/api/newsLetter').then(res => res.json())
-                .then(num => this.setState({ subscribers: num.data }));
-    
-        }
+    componentDidMount() {
+        fetch('/api/newsLetter').then(res => res.json())
+            .then(num => this.setState({ subscribers: num.data }));
+    }
 
-    
+
 
     render() {
-
 
         return (
 
@@ -78,10 +61,6 @@ class Contact_form extends Component {
                     <p>Join {this.state.subscribers} Subscribers to My News Update </p>
                     <p> Promise: No more than 1 mail/month. </p>
                     <form onSubmit={this.subscribe}>
-
-
-                      
-
                         <div className="form-group row">
                             <label htmlFor="inputEmail" className="col-sm-2 col-md-2 col-form-label" >Email *</label>
                             <div className="col-sm-10 col-md-8">
