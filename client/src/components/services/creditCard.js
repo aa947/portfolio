fb9843f56fea285dbd1b0ff2f06b081ce6567142
Ahmad Ali <aallii300300@gmail.com>
2020-05-21 03:06:50 +0100

    clean credit card

--- client/src/components/services/creditCard.js
+++ client/src/components/services/creditCard.js
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css'
 import Footer from '../Footer';
@@ -6,16 +6,17 @@ import '../css/services/creditCard.css';
 
 
 
-function CreditCard () {
-   
-        const [number, setNumber] = useState('');
-        const [name, setName] = useState('');
-        const [expiry, setExpiry] = useState('');
-        const [cvc, setCvc] = useState('');
-        const [focus, setFocus] = useState('');
-        return (
-            <>
-                <div className="col-lg-12 mb-12">
+function CreditCard() {
+
+    const [number, setNumber] = useState('');
+    const [name, setName] = useState('');
+    const [expiry, setExpiry] = useState('');
+    const [cvc, setCvc] = useState('');
+    const [focus, setFocus] = useState('');
+
+    return (
+        <>
+            <div className="col-lg-12 mb-12">
 
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
@@ -23,50 +24,50 @@ function CreditCard () {
                     </div>
                     <div className="card-body">
 
-                <Cards 
-                    number ={ number}
-                    name = { name }
-                    expiry = { expiry }
-                    cvc ={ cvc }
-                    focused = { focus }
-                />
+                        <Cards
+                            number={number}
+                            name={name}
+                            expiry={expiry}
+                            cvc={cvc}
+                            focused={focus}
+                        />
 
-                <br /> <br />
+                        <br /> <br />
 
-                <form className="center_div">
-                    <input 
-                        type="tel"
-                        name="number"
-                        placeholder="Card Number"
-                        value={number}
-                        onChange={e => setNumber(e.target.value)}
-                        onFocus={ e=> setFocus(e.target.name) }
-                    />
-                     <input 
-                        type="text"
-                        name="name"
-                        placeholder="Card Holder Name"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        onFocus={ e=> setFocus(e.target.name) }
-                    />
-                     <input 
-                        type="text"
-                        name="expiry"
-                        placeholder="Card Expiry MM/YY"
-                        value={expiry}
-                        onChange={e => setExpiry(e.target.value)}
-                        onFocus={ e=> setFocus(e.target.name) }
-                    />
-                     <input 
-                        type="tel"
-                        name="cvc"
-                        placeholder="CVC"
-                        value={cvc}
-                        onChange={e => setCvc(e.target.value)}
-                        onFocus={ e=> setFocus(e.target.name) }
-                    />
-                </form>
+                        <form className="center_div">
+                            <input
+                                type="tel"
+                                name="number"
+                                placeholder="Card Number"
+                                value={number}
+                                onChange={e => setNumber(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)}
+                            />
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Card Holder Name"
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)}
+                            />
+                            <input
+                                type="text"
+                                name="expiry"
+                                placeholder="Card Expiry MM/YY"
+                                value={expiry}
+                                onChange={e => setExpiry(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)}
+                            />
+                            <input
+                                type="tel"
+                                name="cvc"
+                                placeholder="CVC"
+                                value={cvc}
+                                onChange={e => setCvc(e.target.value)}
+                                onFocus={e => setFocus(e.target.name)}
+                            />
+                        </form>
 
                     </div>
                 </div>
@@ -77,26 +78,22 @@ function CreditCard () {
                     </div>
                     <div className="card-body">
 
-                    <p style={{fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> Change The data in form Input and the changes will transfomed on the card animation. </p> 
+                        <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> Change The data in form Input and the changes will transfomed on the card animation. </p>
 
-                    <p style={{fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> start Card Number with 55 and will represent its corresponding company.
+                        <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> start Card Number with 55 and will represent its corresponding company.
                     </p>
 
-                    <p style={{fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> this is Just Animation, Do Not Inter your real credit card data.
+                        <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> this is Just Animation, Do Not Inter your real credit card data.
                     </p>
 
                     </div>
                 </div>
+            </div>
+            <Footer />
+        </>
+    );
 
-
-                </div>
-
-                <Footer />
-            </>
-        );
-
-    }
-
+}
 
 
 export default CreditCard;
