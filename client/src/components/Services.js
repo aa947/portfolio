77e9f567392a75b88ec140c35a24647c163630ec
Ahmad Ali <aallii300300@gmail.com>
2020-05-21 03:06:18 +0100

    clean services

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -2,29 +2,18 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Services extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-
-        }
-    }
-
+   
     render() {
         return (
             <div>
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
                         <h6 className="m-0 font-weight-bold text-primary">
-                            Featured Projects,
-                        <span style={{ fontSize: "0.8rem" }}>
-                                All are internal pages in this site
-                        </span>
+                            Featured Projects
+                        
                         </h6>
                     </div>
                     <div className="card-body">
-
-                        {/* <p style={{fontSize: "20px",  marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> <Link to={{pathname: '/api/s/firstAPI' }}> First APi </Link> </p> */}
                         <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> <Link to={{ pathname: '/services/forecast' }}>Weather Forecast </Link> </p>
 
                         <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> <Link to={{ pathname: '/services/creditCard' }}>Credit Card Animation </Link> </p>
@@ -32,15 +21,7 @@ export default class Services extends Component {
                         <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> <Link to={{ pathname: '/services/corona' }}>Corona Virus Live Monitor </Link> </p>
 
                         <p style={{ fontSize: "20px", marginBottom: "1%" }}><i className="fas fa-chevron-right"></i> <Link to={{ pathname: '/services/dg-url' }}> dg-url </Link> </p>
-                      
-
-
-
-
                     </div>
-                    {/* <div className="card-header py-3">
-                        <p style={{ fontSize: "0.8rem" }} className="m-0 font-weight-bold text-primary"> ** Active Pages from this site that use APIs to Give you some valuable Data. </p>
-                    </div> */}
                 </div>
             </div>
         )
