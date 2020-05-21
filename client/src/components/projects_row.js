349f0b1e7d1c68e286d48d461d159990963d2a09
Ahmad Ali <aallii300300@gmail.com>
2020-05-21 03:05:27 +0100

    clean project row

--- client/src/components/projects_row.js
+++ client/src/components/projects_row.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-// import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-//import Jumbotron from 'react-bootstrap/Jumbotron';
 import Contact_form from './Contact_form';
-
-
-
 import './css/intro.css';
 import { Link } from 'react-router-dom';
 
@@ -26,65 +19,35 @@ class Project_row extends Component {
 
     render() {
 
-
         return (
             <div className="col-lg-12 mb-12">
 
                 {/* <!-- Color System --> */}
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
-                        <h6 className="m-0 font-weight-bold text-primary">Prpjects</h6>
+                        <h6 className="m-0 font-weight-bold text-primary">Projects</h6>
                     </div>
                     <div className="card-body">
                         <div className="row">
-
                             {/* <!-- start project row --> */}
 
-
                             {this.state.projects.map((p) => {
+                                return (<div className="col col-12" >
+
+                                    <p style={{ marginBottom: "1%", marginTop: "1%", display: "inline-block" }}><i className="fas fa-chevron-right"></i> <i className={p.fontAwesome}></i>  {p.name} - {p.date}
+
+                                        <a href={p.live_demo} title="visit Live Demo" target="_blank">
+                                            <button className="btn btn-link text-muted">  <i className="fas fa-external-link-alt"> visit</i> </button>
+                                        </a>
 
 
-                                return(<div className="col col-12" >
-                                
-                                <p style={{ marginBottom: "1%", marginTop: "1%", display: "inline-block" }}><i className="fas fa-chevron-right"></i> <i className={p.fontAwesome}></i>  {p.name} - {p.date} 
-                                 
-                                     <a  href={p.live_demo} title="visit Live Demo" target="_blank">
-                                       <button className="btn btn-link text-muted">  <i className="fas fa-external-link-alt"> visit</i> </button>
-                                    </a>
-
-
-                                    {/* {'\u00A0'} */}
-                                    <Link title="View more Details" to={{ pathname: "/p/" + p._id, project_id: p._id }}>  
-                                       <button className="btn btn-link text-muted" >  <i className="fas fa-info-circle"> details</i> </button>
-                                    </Link>
-                                </p>
+                                        {/* {'\u00A0'} */}
+                                        <Link title="View more Details" to={{ pathname: "/p/" + p._id, project_id: p._id }}>
+                                            <button className="btn btn-link text-muted" >  <i className="fas fa-info-circle"> details</i> </button>
+                                        </Link>
+                                    </p>
                                 </div>);
-
-
-
-
                             })}
-
-
-
-
-                            {/* hey */}
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                            </div>
-
-
 
                             {/* <!-- end project row --> */}
                         </div>
@@ -92,11 +55,8 @@ class Project_row extends Component {
                     </div>
                 </div>
 
-            <Contact_form />
-                </div>
-
-
-
+                <Contact_form />
+            </div>
         );
     }
 }
