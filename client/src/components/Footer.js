30d22d535c6156787bcd781b74ea966b11992d66
Ahmad Ali <aallii300300@gmail.com>
2020-05-21 03:03:14 +0100

    clean footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,43 +1,37 @@
 import React, { Component } from 'react';
 import './css/header.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
 
 class Footer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            count: 0,
-            unique:0
-        };
-
-    }
-
-    componentDidMount() {
-      fetch('/api/footer').then(res => res.json())
-          .then(res => this.setState({ count: res.count, unique: res.unique }  ));
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      unique: 0
+    };
 
   }
 
-    render() {
-        return (
-            <React.Fragment>
+  componentDidMount() {
+    fetch('/api/footer').then(res => res.json())
+      .then(res => this.setState({ count: res.count, unique: res.unique }));
 
-<footer className="sticky-footer bg-white">
-        <div>
-          <div className="copyright text-center my-auto">
-            <span>Copyright &copy; Ahmad Ali </span> {'\u00A0'}
-            <span title="visits number"><i className="fas fa-eye">  { this.state.count }</i>  </span> {'\u00A0'}
-            <span title="number of unique visitors"><i className="fas fa-fingerprint"> { this.state.unique }</i>  </span>{'\u00A0'}
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <footer className="sticky-footer bg-white">
+          <div>
+            <div className="copyright text-center my-auto">
+              <span>Copyright &copy; Ahmad Ali </span> {'\u00A0'}
+              <span title="visits number"><i className="fas fa-eye">  {this.state.count}</i>  </span> {'\u00A0'}
+              <span title="number of unique visitors"><i className="fas fa-fingerprint"> {this.state.unique}</i>  </span>{'\u00A0'}
+            </div>
           </div>
-        </div>
-      </footer>
-            </React.Fragment>
-        );
-    }
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Footer;
