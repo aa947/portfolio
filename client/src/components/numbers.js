268ea51eee3a3fed586e0f49f6c88458c63777ff
Ahmad Ali <aallii300300@gmail.com>
2020-05-22 21:51:14 +0100

    fixin links

--- client/src/components/numbers.js
+++ client/src/components/numbers.js
@@ -50,11 +50,11 @@ class Numbers extends Component {
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
                   <div className="text-sm font-weight-bold text-info text-uppercase mb-1">
-                    <a href="#"> My Growth Report </a>
+                    <a href="/report"> My Growth Report </a>
                   </div>
                   <div className="row no-gutters align-items-center">
                     <div className="col-auto">
-                      <div className="p mb-0 mr-3 font-weight-bold text-gray-800">coming soon ..</div>
+                      <div className="p mb-0 mr-3 font-weight-bold text-gray-800">my growth on data</div>
                     </div>
                     <div className="col">
                       {/* <div className="progress progress-sm mr-2">
