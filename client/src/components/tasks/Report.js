1f1a39f53dad47988b469ffc4ce9e98535ebc8b5
Ahmad Ali <aallii300300@gmail.com>
2020-05-22 21:52:28 +0100

    add projects chart

--- client/src/components/tasks/Report.js
+++ client/src/components/tasks/Report.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import Chart from "react-google-charts";
 import axios from 'axios';
 import Footer from '../Footer';
-import { fetchUser, fetchRepos } from './report/fetching_functions';
+import { fetchUser, fetchRepos, fetchProjects } from './report/fetching_functions';
 import { renderCard,  } from './report/rendering_functions';
-import { githubData, extractRepoLanguages, languageChart } from './report/charts_functions';
+import { githubData, extractRepoLanguages, languageChart, extractProjectsData, projectsChart } from './report/charts_functions';
+import tempProjectsJson from './report/reportData/projects.json';
 
 export default class Report extends Component {
 
@@ -19,8 +20,9 @@ export default class Report extends Component {
                 member: 2,
                 total: 0
             },
-            repos: []
+            repos: [],
             //githubLoading: true,
+            projects : [], 
         }
 
         //this.renderCard = this.renderCard.bind(this);
@@ -31,16 +33,23 @@ export default class Report extends Component {
     componentDidMount() {
         fetchUser(this);
         fetchRepos(this);
+        fetchProjects(this);
     }
     render() {
+
+     
+          
        let languageData =  this.state.repos.length > 0 ?  extractRepoLanguages(this.state.repos) : {HTML: 122053 , JavaScript: 1487077, Python: 67013, Ruby: 34405, Shell: 4605  };
 
-       let githubUserData =  this.state.repos.length > 0 ?  this.state.github : { followers: 10, following: 135, public_repos: 67, public_gists: 1, total:225, member:2 };
+       let githubUserData =  this.state.github.followers > 0 ?  this.state.github : { followers: 10, following: 135, public_repos: 67, public_gists: 1, total:225, member:2 };
+
+       let projectsData = this.state.projects.length > 0 ? extractProjectsData(this.state.projects) : extractProjectsData(tempProjectsJson);
         return (
             
             <>
                 {renderCard('My Github Account Overview', githubData(githubUserData))}
                 {renderCard('My Programming Languages\' details', languageChart(languageData))}
+                {renderCard('My Projects\' details', projectsChart(projectsData))}
                 <Footer />
             </>
         )
