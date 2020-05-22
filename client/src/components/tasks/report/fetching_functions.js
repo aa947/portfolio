bf4b2102930af5682f56c899a812fa042b72b9d4
Ahmad Ali <aallii300300@gmail.com>
2020-05-22 16:30:32 +0100

    data fetcing functions for report.js

---
+++ client/src/components/tasks/report/fetching_functions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+/**
+ * 
+ * @param {*} that <Class Copmonent> copmonent to work on.
+ * @desc  fetching github user and updates state.github, state.githubLoading
+ */
+export function fetchUser(that){
+    axios.get('https://api.github.com/users/ahmad-ali14')
+    .then((res) => {
+        let { followers, following, public_repos, public_gists } = res.data;
+        let total = followers + following + public_repos + public_gists + 2;
+        that.setState({ github: { followers, following, public_repos, public_gists, total, member: 2 } })
+    }).catch(err => console.log(err))
+}
+
+/**
+ * 
+ * @param {*} that <Class Copmonent> copmonent to work on.
+ * @desc  fetching Repos
+ */
+export function fetchRepos(that){
+    axios.get('https://api.github.com/users/ahmad-ali14/repos')
+    .then((res) => {
+        console.log(res.data)
+        that.setState({repos : res.data})
+    }).catch(err => console.log(err))
+}
+
