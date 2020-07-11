b5970a1bcc0007f709aeacc0a5ba5d60d1a2ee85
Ahmad Ali <aallii300300@gmail.com>
2020-07-11 15:55:45 +0100

    top text card

---
+++ client/src/cards/TopTextCard.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const TopTextCard = ({ title, text, color, cardLink, iconClass }) => {
+
+    return (
+        <div className="col-xl-3 col-md-6 mb-4">
+            <div className={`card border-left-${color} shadow h-100 py-2`}>
+                <div className="card-body">
+                    <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                            <div className="text-sm font-weight-bold text-primary text-uppercase mb-1">
+                                <a href={cardLink}> {title} </a>
+                            </div>
+                            <div className="p mb-0 font-weight-bold text-gray-800">{text}</div>
+                        </div>
+                        <div className="col-auto">
+                            <i className={`${iconClass} fa-2x text-gray-300`}></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+
+export default TopTextCard;
