fd3873f446b3b740c6ade0d8f17cd7040fef2d7a
Ahmad Ali <aallii300300@gmail.com>
2020-07-11 10:34:04 +0100

    / path route

--- client/src/App.js
+++ client/src/App.js
@@ -99,6 +99,7 @@ class App extends Component {
               *
               * Blog Routes
               */}
+            <Route path="/"> <Home /> </Route>
             <Route exact path="/blog"> <Blog /> </Route>
             <Route exact path="/blog/posts/connecting-your-app-to-mongodb-atlas">
               <Connecting_to_Atlas />
