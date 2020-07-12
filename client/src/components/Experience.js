16a83502c907c64aabda13259a1f8ce248e9e8e8
Ahmad Ali <aallii300300@gmail.com>
2020-07-12 22:21:24 +0100

    add code your future experience

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ExperienceCard from '../cards/ExperienceCard';
+import codeYourFuture from './experiences/codeYourFuture';
 
 const Experience = () => {
-
-    const codeYourFuture = () => {
-        return (<ul>
-            <li>teaching students a full-stack 8-month long course.</li>
-            <li>building the internal infrastructure which contains more than 6 apps that support and organise more than 1000 students and volunteers in more than 10 cities around the world.</li>
-            <li> created projects so the students can work on them as homework.</li>
-            <li>reviewed students code and gave them advice following the best practices.</li>
-            <li> I have more than 2000 lines in production.</li>
-        </ul>)
-    }
 
     return (
         <div>
@@ -28,13 +19,14 @@ const Experience = () => {
                             color="primary"
                             title="code Your Future"
                             text="Full Stack Developer"
-                            iconClass="fas fa-calendar-alt"
                             TextComponent={codeYourFuture}
                             imageUrl="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png"
                             image="rect"
+                            date="April 2020 - present"
+                            place="London"
                         />
 
-                        <ExperienceCard
+                        {/* <ExperienceCard
                             orienting="right"
                             cardLink="https://docs.ahmad-ali.co.uk/"
                             color="success"
@@ -65,7 +57,7 @@ const Experience = () => {
                             iconClass="fas fas fa-bullhorn"
                             TextComponent={codeYourFuture}
                             imageUrl="https://dl.dropbox.com/s/2kqh712gqev9yhy/ahmad-ali-200-200.jpg?dl=0"
-                        />
+                        /> */}
                     </div>
                 </div>
             </div>
