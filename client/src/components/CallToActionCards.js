da6aae0e22ee8857387dec51a168b5419d38b129
Ahmad Ali <aallii300300@gmail.com>
2020-07-13 00:02:05 +0100

    cal to actions styling

--- client/src/components/CallToActionCards.js
+++ client/src/components/CallToActionCards.js
@@ -10,39 +10,42 @@ class CallToActionCards extends Component {
   render() {
     return (
       <React.Fragment>
+        <div>
+          <div className="row" >
 
-        <TopTextCard
-          cardLink="/meetings"
-          color="primary"
-          title="Book a Meeting"
-          text="Get a Free Quote"
-          iconClass="fas fa-calendar-alt"
-        />
+            <TopTextCard
+              cardLink="/meetings"
+              color="primary"
+              title="Book a Meeting"
+              text="Get a Free Quote"
+              iconClass="fas fa-calendar-alt"
+            />
 
-        <TopTextCard
-          cardLink="https://docs.ahmad-ali.co.uk/"
-          color="success"
-          title="Activity Docs"
-          text="Quick CheatSheets"
-          iconClass="fas fa-user-cog"
-        />
+            <TopTextCard
+              cardLink="https://docs.ahmad-ali.co.uk/"
+              color="success"
+              title="Activity Docs"
+              text="Quick CheatSheets"
+              iconClass="fas fa-user-cog"
+            />
 
-        <TopTextCard
-          cardLink="/report"
-          color="info"
-          title="My Report"
-          text="Work Summary"
-          iconClass="fas fa-chart-line"
-        />
+            <TopTextCard
+              cardLink="/report"
+              color="info"
+              title="My Report"
+              text="Work Summary"
+              iconClass="fas fa-chart-line"
+            />
 
-        <TopTextCard
-          cardLink="/reviews"
-          color="warning"
-          title="Add a Review"
-          text="Review My Work"
-          iconClass="fas fas fa-bullhorn"
-        />
-
+            <TopTextCard
+              cardLink="/reviews"
+              color="warning"
+              title="Add a Review"
+              text="Review My Work"
+              iconClass="fas fas fa-bullhorn"
+            />
+          </div>
+        </div>
       </React.Fragment>
     );
   }
