d1544d7637f1bbbca56bbd8a6dc94e38d7e9bb68
Ahmad Ali <aallii300300@gmail.com>
2020-07-18 16:22:06 +0100

    online edu

---
+++ client/src/components/OnlineEducation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import OnlineEducationCard from '../cards/OnlineCourseCard';
+import OnlineEducationData from '../data/certificates.json';
+
+import { isLocalhost } from '../helpers';
+
+
+const OnlineEducation = ({ chooseSideColor }) => {
+    const certUrl = isLocalhost ? "http://localhost:3000/certificates/" : 'https://www.ahmad-ali.co.uk/certificates/';
+
+
+    return (
+        <div>
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="m-0 font-weight-bold text-primary">Online Education</h6>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+
+                        {
+                            OnlineEducationData.map(pr =>
+
+                                <OnlineEducationCard
+                                    courseTitle={pr.cert_name}
+                                    detailsLink={certUrl + pr._id.$oid}
+                                    color={chooseSideColor()}
+                                    courseLink={pr.course_url}
+
+                                />
+                            )
+                        }
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default OnlineEducation;
