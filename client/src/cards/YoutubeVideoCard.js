86c28cf872abb0cdce24ce63db2eab04520a087b
Ahmad Ali <aallii300300@gmail.com>
2020-07-18 16:19:42 +0100

    youtube

---
+++ client/src/cards/YoutubeVideoCard.js
@@ -0,0 +1,38 @@
+
+
+
+import React from 'react';
+
+const OnlineCourseCard = ({ title, videoLink, color }) => {
+
+    return (
+        <div className="col-xl-4 col-md-4 col-sm-6 col-12 mb-4">
+            <div className={`card  shadow h-100 py-2`}>
+                <div className="card-body">
+                    <div className="row no-gutters align-items-center">
+                        {title && <div className="col mr-2">
+                            <div className="text-sm font-weight-bold text-primary text-uppercase mb-1">
+                                {/* <a href={courseLink}> {title} </a> */}
+                                <iframe src={videoLink}
+                                    frameborder='0'
+                                    allow='autoplay; encrypted-media'
+                                    allowfullscreen
+                                    title='video'
+                                    width="100%"
+                                />
+                                {/* {/* <iframe className='video' width="560" height="315" src={videoLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+                            </div>
+                            <div className="text-sm font-weight-bold text-black text-uppercase mb-1">{title}</div>
+                            <div className="p mb-0 text-xs text-gray-800"><a href={videoLink}> view on Yotube</a></div>
+
+                        </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default OnlineCourseCard;
---
+++ client/src/components/YoutubeChannel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import YoutubeVideoCard from '../cards/YoutubeVideoCard';
+import youtubeVideosData from '../data/youtubevideos.json';
+
+import { isLocalhost } from '../helpers';
+
+
+const YoutubeChannel = ({ chooseSideColor }) => {
+
+
+
+    return (
+        <div>
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="m-0 font-weight-bold text-primary">YouTube Channel</h6>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+
+                        {
+                            youtubeVideosData.map(pr =>
+
+                                <YoutubeVideoCard
+                                    title={pr.title}
+                                    color={chooseSideColor()}
+                                    videoLink={pr.link}
+
+                                />
+                            )
+                        }
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default YoutubeChannel;
