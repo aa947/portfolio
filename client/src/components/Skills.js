03d4709426a14cc812f70d12211953fd7b28d7ce
Ahmad Ali <aallii300300@gmail.com>
2020-07-18 11:05:22 +0100

    kills component

---
+++ client/src/components/Skills.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SkillCard from '../cards/SkillCard';
+import skillsData from '../data/skills.json'
+
+const Skills = () => {
+
+    return (
+        <div>
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="m-0 font-weight-bold text-primary">Skills</h6>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+
+                        {
+                            skillsData.map(skill =>
+
+                                <SkillCard
+                                    color={skill.color}
+                                    title={skill.skill}
+                                    iconClass={skill.fontAwesome}
+                                    image={skill.image}
+                                />
+                            )
+                        }
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Skills;
