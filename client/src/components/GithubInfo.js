4083024e2d07820a9fcf2a8769cd802d336af487
ahmad-ali14 <aallii300300@gmail.com>
2021-02-15 10:48:22 +0000

    update github

--- client/src/components/GithubInfo.js
+++ client/src/components/GithubInfo.js
@@ -10,22 +10,22 @@ function GithubInfo() {
 
         <div className="card-body">
           <div className="row">
-            <div className="col-12">
+            {/* <div className="col-12">
               <img
                 //  align="left"
                 width="100%"
                 // height="300px"
                 alt="Ahmad Ali's Github Stats"
-                src="https://github-readme-stats.vercel.app/api?username=ahmad-ali14&count_private=true&show_icons=true&hide_border=true&theme=tokyonight"
+                src="https://github-readme-stats.vercel.app/api?username=ahmad-ali14&count_private=true&show_icons=true&hide_title=true&hide=stars"
               />
-            </div>
+            </div> */}
             <div className="col-12">
               <img
                 //  align="right"
                 width="100%"
                 // height="300px"
                 alt=""
-                src="https://github-readme-stats.vercel.app/api/top-langs/?username=ahmad-ali14&layout=compact&theme=tokyonight&hide_border=true"
+                src="https://github-profile-trophy.vercel.app/?username=ahmad-ali14&theme=flat&no-frame=true&margin-w=30"
               />
             </div>
           </div>
